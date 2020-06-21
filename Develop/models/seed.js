e7df75997c7673225e17c3fae550c165e4ee4737
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-06-21 12:52:41 -0400

    adding seed into models folder

--- Develop/models/seed.js
+++ Develop/models/seed.js
@@ -1,10 +1,10 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+let db = require("./Workout");
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 let workoutSeed = [
   {
