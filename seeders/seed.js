d06c4f344f15fca12823dc1c645f4b4307f7f9f6
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-06-21 13:56:50 -0400

    adding seeds

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,4 +1,10 @@
-let db = require("./Workout");
+let mongoose = require("mongoose");
+let db = require("../models");
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 let workoutSeed = [
   {
